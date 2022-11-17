49d8525eaf0f208f960ab03f58169583b0bb85d3
a.artamonov <a.artamonov@gb.ru>
2022-11-17 22:34:23 +0300

    Second task is done.

--- Program.cs
+++ Program.cs
@@ -4,34 +4,87 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-double[,] Create2dArray()
-{
-    Console.Write("Input numbers of rows: ");
-    int rows = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input numbers of columns: ");
-    int columns = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input min possible value: ");
-    int minValue = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input max possible value: ");
-    int maxValue = Convert.ToInt32(Console.ReadLine());
+// double[,] Create2dArray()
+// {
+//     Console.Write("Input numbers of rows: ");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Input numbers of columns: ");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Input min possible value: ");
+//     int minValue = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Input max possible value: ");
+//     int maxValue = Convert.ToInt32(Console.ReadLine());
 
-    double[,] createdArray = new double[rows, columns];
+//     double[,] createdArray = new double[rows, columns];
+
+//     for (int i = 0; i < rows; i++)
+//         for (int j = 0; j < columns; j++)
+//             createdArray[i, j] = Math.Round(new Random().Next(minValue, maxValue + 1) + new Random().NextDouble(), 1);
+
+//     return createdArray;        
+// }
+
+// void Show2dArray (double[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//             Console.Write(array[i, j] + " ");
+//         Console.WriteLine();
+//     }     
+// }
+
+// Show2dArray(Create2dArray());
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+int[,] Create2dArray()
+{
+    int rows = new Random().Next(1, 5);
+    int columns = new Random().Next(1, 5);
+    int[,] createdArray = new int[rows, columns];
 
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < columns; j++)
-            createdArray[i, j] = Math.Round(new Random().Next(minValue, maxValue + 1) + new Random().NextDouble(), 1);
+            createdArray[i, j] = new Random().Next(-9, 10);
 
-    return createdArray;        
+    return createdArray;
 }
 
-void Show2dArray (double[,] array)
+void Show2dArray (int[,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
         for(int j = 0; j < array.GetLength(1); j++)
             Console.Write(array[i, j] + " ");
         Console.WriteLine();
-    }     
+    }
 }
 
-Show2dArray(Create2dArray());
+void FindElemFromPosition(int[,] array, int rowFind, int columnFind)
+{
+    if(rowFind < array.GetLength(0) && columnFind < array.GetLength(1))
+        Console.WriteLine($"There is your element with index [{rowFind}, {columnFind}]: {array[rowFind, columnFind]}");
+    else
+        Console.WriteLine($"There is not fond with your index [{rowFind}, {columnFind}] ");
+}
+
+Console.Write("Input number of row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of column: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+if(row >= 0 && column >= 0)
+{
+    int[,] randArray = Create2dArray();
+    Show2dArray(randArray);
+    FindElemFromPosition(randArray, row, column);
+}
+else Console.WriteLine("Index cant be less than 0. Please input valid index.");
